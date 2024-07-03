4364edb690e9dc542760e2fd4332f25e73472ece
Danny Ayers <danny.ayers@gmail.com>
2024-07-03 18:12:17 +0200

    bits

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@ git pull
 cp /home/github/fuseki-ldap/custom-ldap-realm.jar /home/services/fuseki/extra/
 rm /var/log/fuseki/*
 systemctl restart fuseki
-head -n 20 /var/log/fuseki/fuseki-debug.log 
+# head -n 20 /var/log/fuseki/fuseki-debug.log 
